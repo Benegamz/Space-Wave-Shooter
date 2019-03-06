a178665b3db973242569f9fb42e6abefd9b445a3
Manugamez <manuel@pielka.de>
2019-03-06 15:41:40 +0100

    Additional Work with Player Rotation

--- Space-Wave-Shooter/Assets/Scripts/PlayerMovement.cs
+++ Space-Wave-Shooter/Assets/Scripts/PlayerMovement.cs
@@ -4,42 +4,53 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float turnSpeed = 1;
+    public int acceleration = 100;
+    public int maxSpeed = 1000;
 
     void Start()
     {
-        
+       
     }
 
 
     void Update()
     {
-        if (Input.GetAxis("Mouse X")<0)
+        if (Input.GetKey("left alt")!= true)
         {
-            Debug.Log("Mouse moved left");
-            transform.Rotate(0,-1,0,Space.Self);
+            Cursor.visible = false;
+            if (Input.GetAxis("Mouse X")<0)
+            {
+                Debug.Log("Mouse moved left");
+                transform.Rotate(0,-1*turnSpeed*-Input.GetAxis("Mouse X"),0,Space.Self);
+            }
+            if (Input.GetAxis("Mouse X")>0)
+            {
+                Debug.Log("Mouse moved right");
+                transform.Rotate(0,1*turnSpeed*Input.GetAxis("Mouse X"),0,Space.Self);
+            }
+            if (Input.GetAxis("Mouse Y")<0)
+            {
+                Debug.Log("Mouse moved down");
+                transform.Rotate(-2*turnSpeed*-Input.GetAxis("Mouse Y"),0,0,Space.Self);
+            }
+            if (Input.GetAxis("Mouse Y")>0)
+            {
+                Debug.Log("Mouse moved up");
+                transform.Rotate(2*turnSpeed*Input.GetAxis("Mouse Y"),0,0,Space.Self);
+            }
+            if (Input.GetKey("q"))
+            {
+                transform.Rotate(0,0,2*turnSpeed,Space.Self);
+            }
+            if (Input.GetKey("e"))
+            {
+                transform.Rotate(0,0,-2*turnSpeed,Space.Self);
+            }
         }
-        if (Input.GetAxis("Mouse X")>0)
+        else
         {
-            Debug.Log("Mouse moved right");
-            transform.Rotate(0,1,0,Space.Self);
-        }
-        if (Input.GetAxis("Mouse Y")<0)
-        {
-            Debug.Log("Mouse moved down");
-            transform.Rotate(-1,0,0,Space.Self);
-        }
-        if (Input.GetAxis("Mouse Y")>0)
-        {
-            Debug.Log("Mouse moved up");
-            transform.Rotate(1,0,0,Space.Self);
-        }
-        if (Input.GetKey("q"))
-        {
-            transform.Rotate(0,0,1);
-        }
-        if (Input.GetKey("e"))
-        {
-            transform.Rotate(0,0,-1);
+            Cursor.visible = true;
         }
     }
 }
