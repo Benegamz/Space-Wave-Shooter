30806c230e48e51b72bcbd4ebf000329f9884b18
Manugamez <manuel@pielka.de>
2019-03-06 19:06:37 +0100

    Bullets Working

---
+++ Space-Wave-Shooter/Assets/Scripts/GunBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBehaviour : MonoBehaviour
+{
+    public GameObject Bullet;
+    float cooldownTimer = 0f;
+    void Start()
+    {
+     InvokeRepeating("Cooldown", 0, 0.1f);   
+    }
+
+    
+    void Update()
+    {
+        if (cooldownTimer <= 0)
+        {
+            if (Input.GetKey("mouse 0"))
+            {
+                Instantiate(Bullet, transform.position + (transform.forward * 10), transform.rotation);
+                cooldownTimer = 1.5f;
+            }
+        }
+    }
+    void Cooldown ()
+    {
+        cooldownTimer--;
+    }
+    
+}
