686cffc2b0f8d168fc724a29b1afbb095b405fdc
Manugamez <manuel@pielka.de>
2019-03-06 13:09:21 +0100

    Update PlayerMovement.cs

--- Space-Wave-Shooter/Assets/Scripts/PlayerMovement.cs
+++ Space-Wave-Shooter/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (Input.GetAxis("Mouse X")<0)
+        {
+            Debug.Log("Mouse moved left");
+            transform.Rotate(0,-1,0,Space.Self);
+        }
+        if (Input.GetAxis("Mouse X")>0)
+        {
+            Debug.Log("Mouse moved right");
+            transform.Rotate(0,1,0,Space.Self);
+        }
+        if (Input.GetAxis("Mouse Y")<0)
+        {
+            Debug.Log("Mouse moved down");
+            transform.Rotate(-1,0,0,Space.Self);
+        }
+        if (Input.GetAxis("Mouse Y")>0)
+        {
+            Debug.Log("Mouse moved up");
+            transform.Rotate(1,0,0,Space.Self);
+        }
+        if (Input.GetKey("q"))
+        {
+            transform.Rotate(0,0,1);
+        }
+        if (Input.GetKey("e"))
+        {
+            transform.Rotate(0,0,-1);
+        }
     }
 }
