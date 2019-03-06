a97334024e76b06e7f38037fe9d91b7aa5826586
Benegamz <benegamz@t-online.de>
2019-03-06 17:09:56 +0100

    Added Line between enemy and player

---
+++ Space-Wave-Shooter/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{   
+    public GameObject player;
+
+    Transform enemyTR;
+
+    void Start()
+    {
+        Transform enemyTR = GetComponent<Transform>();
+
+    }
+
+    void Update()
+    {
+        Vector3 playerPos = player.transform.position;
+
+        Vector3 direction = player.transform.position - transform.position;
+        
+        Debug.DrawLine(transform.position,player.transform.position, Color.red);
+
+    }
+}
