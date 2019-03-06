b565cbf7f5d68a5204239cf490baba432dbdc83c
Benegamz <benegamz@t-online.de>
2019-03-06 20:01:34 +0100

    Things

---
+++ Space-Wave-Shooter/Assets/Scripts/TurretShooting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretShooting : MonoBehaviour
+{   
+
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        float distance = Vector3.Distance(transform.position,Player.transform.position);
+        if(distance <=5) {
+            Fire();
+            Debug.Log("In Range");
+        }        
+    }
+
+    void Fire() {
+
+    }
+
+}
