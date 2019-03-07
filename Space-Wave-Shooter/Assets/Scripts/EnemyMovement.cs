91b9e52bd78d241a6a9c09f1f866918e861c7f8c
Benegamz <benegamz@t-online.de>
2019-03-07 15:01:08 +0100

    Added AIII

--- Space-Wave-Shooter/Assets/Scripts/EnemyMovement.cs
+++ Space-Wave-Shooter/Assets/Scripts/EnemyMovement.cs
@@ -6,18 +6,23 @@ public class EnemyMovement : MonoBehaviour
 {   
     public GameObject player;
 
+    public float speed;
+
     void Start()
     {
-
+        player = GameObject.Find("Player");
     }
 
     void Update()
     {
 
         Vector3 direction = player.transform.position - transform.position;
-        direction.Normalize();
         
         Debug.DrawRay(transform.position, direction, Color.red);
+        
+        float step = speed * Time.deltaTime;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward,direction,step,0.0f);
 
+        transform.rotation = Quaternion.LookRotation(newDir);
     }
 }
