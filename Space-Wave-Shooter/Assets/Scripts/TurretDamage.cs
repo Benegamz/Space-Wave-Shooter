38c88cd5ba28a8fd5a885dae88251cef9fac499e
Manugamez <manuel@pielka.de>
2019-03-07 15:01:53 +0100

    added Turretdamage

---
+++ Space-Wave-Shooter/Assets/Scripts/TurretDamage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretDamage : MonoBehaviour
+{
+
+    public int currentHp = 50;
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.tag == "Bullet")
+        {
+            currentHp = currentHp - 10;
+            if (currentHp <= 0)
+            {
+                Destroy (gameObject);
+            }
+        }
+    }
+}
