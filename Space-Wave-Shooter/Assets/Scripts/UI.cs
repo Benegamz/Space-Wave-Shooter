9d949be7a509db322ee4ee547e4f223d4856d7cb
Manugamez <manuel@pielka.de>
2019-03-07 18:16:16 +0100

    asd

--- Space-Wave-Shooter/Assets/Scripts/UI.cs
+++ Space-Wave-Shooter/Assets/Scripts/UI.cs
@@ -7,13 +7,16 @@ public class UI : MonoBehaviour
 {
     public PlayerMovement playerMovement;
     public PlayerDamage playerDamage;
+    public GunBehaviour missileStatus;
     public Text speedText;
     public Text hpText;
+    public Text missileText;
 
     // Update is called once per frame
     void Update()
     {
         speedText.text = "Speed:" + playerMovement.CurrentSpeed;
         hpText.text = "Hp:" + playerDamage.currentHp;
+        missileText.text = "Missiles:" + missileStatus.missileAmount;
     }
 }
